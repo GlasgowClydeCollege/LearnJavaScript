e385de008087028808835c2e5100ff452d3a3225
daveball <dave.mike.ball@gmail.com>
2016-11-27 22:27:33 +0000

    added all file and  directories for the  beginning of the  tutorial

--- tests/Helper.js
+++ tests/Helper.js
@@ -1,3 +1,31 @@
 /**
  * Created by davem on 27/11/2016.
  */
+var fixture;
+
+function loadFixture(path) {
+    var html;
+    jQuery.ajax({
+        url: '/index.html',
+        success: function(result) {
+            html = result;
+        },
+        async: false
+    });
+    return $.parseHTML(html);
+}
+
+function resetFixture() {
+    if (!fixture) {
+        var index = $('<div>').append(loadFixture('/index.html'));
+        var markup = index.find('div.markup');
+        fixture = $('<div class="fixture" style="display: none">').append(markup);
+        $('body').append(fixture.clone());
+    } else {
+        $('.fixture').replaceWith(fixture.clone());
+    }
+}
+
+beforeEach(function () {
+    resetFixture();
+});
