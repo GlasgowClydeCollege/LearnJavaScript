94d835bd4eaefc42b77a61de87c110ab246f3bd8
daveball <dave.mike.ball@gmail.com>
2016-12-08 15:06:37 +0000

    first test written and  being passed

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,11 @@
 /**
  * Created by davem on 27/11/2016.
  */
-use 'strict';
+'use strict';
 var learnjavascript={};
 
-learnjavascript.showView =function(){
-    var questionView =  function(hash){
-      $('view-container').empty.append(questionView);
+learnjavascript.showView = function(hash){
+    var questionView =  $('<div class="question-view">').text('Arriving Soon!');
+      $('.view-container').empty().append(questionView);
     };
-};
+
--- tests/appSpec.js
+++ tests/appSpec.js
@@ -1,9 +1,9 @@
 /**
  * Created by davem on 27/11/2016.
  */
-describe('learn javascript'), function(){
- it('can show  question  view', function()
-    {   learnjavascript.showView('#question-1');
-        expect($('.view-container'.question - view).length).toEqual(1);
-    };
-};
+describe('Learn JavaScript', function() {
+    it('can show question view', function () {
+        learnjavascript.showView('#question-1');
+        expect($('.view-container .question-view').length).toEqual(1);
+    });
+});
