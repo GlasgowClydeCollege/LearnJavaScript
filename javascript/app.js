98a8cf36ad1ba52550b016e7ade2e9085cadc263
dball <dave.mike.ball@gmail.com>
2016-12-08 11:12:38 +0000

    written  first test

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,11 @@
 /**
  * Created by davem on 27/11/2016.
  */
+use 'strict';
+var learnjavascript={};
+
+learnjavascript.showView =function(){
+    var questionView =  function(hash){
+      $('view-container').empty.append(questionView);
+    };
+};
--- tests/Helper.js
+++ tests/Helper.js
@@ -5,8 +5,8 @@ var fixture;
 
 function loadFixture(path) {
     var html;
-    jQuery.ajax({
-        url: '/index.html',
+    $.ajax({
+        url: '../index.html',
         success: function(result) {
             html = result;
         },
@@ -17,7 +17,7 @@ function loadFixture(path) {
 
 function resetFixture() {
     if (!fixture) {
-        var index = $('<div>').append(loadFixture('/index.html'));
+        var index = $('<div>').append(loadFixture('../index.html'));
         var markup = index.find('div.markup');
         fixture = $('<div class="fixture" style="display: none">').append(markup);
         $('body').append(fixture.clone());
